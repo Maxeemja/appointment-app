231e96a19a9da06663dfc7f38a4b125cafb3bd6e
Max Hrytsiuk <maxeemja.inc@gmail.com>
2024-09-24 20:03:23 +0300

    feat: adding/deleting items, prettier added

--- src/app/components/appointment-list/appointment-list.component.ts
+++ src/app/components/appointment-list/appointment-list.component.ts
@@ -7,9 +7,33 @@ import { Appointment } from '../../models/appointment';
   styleUrl: './appointment-list.component.scss',
 })
 export class AppointmentListComponent {
-  appointment: Appointment = {
-    id: 1,
-    title: 'Buy a bread',
-    date: new Date(),
-  };
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = [
+    {
+      id: 1,
+      title: 'Buy a bread',
+      date: new Date(),
+    },
+  ];
+
+  addItem() {
+    if (this.newAppointmentTitle && this.newAppointmentDate) {
+      const newItem: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+
+      this.appointments.push(newItem);
+
+      this.newAppointmentDate = new Date();
+      this.newAppointmentTitle = '';
+    }
+  }
+
+  deleteAppointment(idx: number) {
+    this.appointments = this.appointments.filter((_, index) => index !== idx);
+  }
 }
